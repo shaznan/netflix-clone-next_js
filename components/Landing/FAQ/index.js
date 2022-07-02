b0d8d8d3710ca8f8aee7fc8054065f1384035d03
shaznan <shaznanfairozesally@gmail.com>
2022-07-02 19:18:30 +0530

    Landing screen: code update

--- components/Landing/FAQ/index.js
+++ components/Landing/FAQ/index.js
@@ -16,8 +16,8 @@ const FAQData = [
     title: "What is Netflix?",
     body: [
       "Netflix is a streaming service that offers a wide variety of award-winning TV shows, movies, anime, documentaries, and more on thousands of internet-connected devices. ",
-      <br />,
-      <br />,
+      // <br />,
+      // <br />,
       " You can watch as much as you want, whenever you want without a single commercial – all for one low monthly price. There's always something new to discover and new TV shows and movies are added every week!",
     ],
   },
@@ -29,8 +29,8 @@ const FAQData = [
     title: "Where can I watch?",
     body: [
       "Watch anywhere, anytime. Sign in with your Netflix account to watch instantly on the web at netflix.com from your personal computer or on any internet-connected device that offers the Netflix app, including smart TVs, smartphones, tablets, streaming media players and game consoles.",
-      <br />,
-      <br />,
+      // <br />,
+      // <br />,
       "You can also download your favorite shows with the iOS, Android, or Windows 10 app. Use downloads to watch while you're on the go and without an internet connection. Take Netflix with you anywhere.",
     ],
   },
@@ -44,12 +44,7 @@ const FAQData = [
   },
   {
     title: "Is Netflix good for kids?",
-    body: [
-      "The Netflix Kids experience is included in your membership to give parents control while kids enjoy family-friendly TV shows and movies in their own space.",
-      <br />,
-      <br />,
-      " Kids profiles come with PIN-protected parental controls that let you restrict the maturity rating of content kids can watch and block specific titles you don’t want kids to see.",
-    ],
+    body: "The Netflix Kids experience is included in your membership to give parents control while kids enjoy family-friendly TV shows and movies in their own space, Kids profiles come with PIN-protected parental controls that let you restrict the maturity rating of content kids can watch and block specific titles you don’t want kids to see.",
   },
 ];
 
