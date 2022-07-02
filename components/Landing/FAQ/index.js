561352f99c992f525bfb21fd63a41b61d6393e8d
shaznan <shaznanfairozesally@gmail.com>
2022-07-02 10:57:51 +0530

    WIP

---
+++ components/Landing/FAQ/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { HeroContainer, HeroWrapper } from "../style";
+import { Title } from "../../common/Title/Title";
+
+const FAQ = () => {
+  return (
+    <HeroContainer>
+      .
+      <HeroWrapper>
+        <Title type="primary">Frequently asked questions</Title>
+      </HeroWrapper>
+    </HeroContainer>
+  );
+};
+
+export default FAQ;
