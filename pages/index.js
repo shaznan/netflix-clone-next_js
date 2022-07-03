2f6ca1963edbc03faa1e3d7ca992dbbe2b6a6f9e
shaznan <shaznanfairozesally@gmail.com>
2022-07-03 20:39:40 +0530

    Add getStaticProps to landing page

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import MainSection from "../components/Landing/MainSection";
 import OurStorySection from "../components/Landing/OurStorySection";
 import FAQ from "../components/Landing/FAQ";
+import axios from "axios";
 
 export default function Landing() {
   return (
@@ -10,4 +11,18 @@ export default function Landing() {
       <FAQ />
     </div>
   );
+}
+
+export async function getStaticProps() {
+  try {
+    const data = await axios.get(
+      "https://aqakub0ie3.execute-api.us-east-1.amazonaws.com/api/v1/landing/faq"
+    );
+
+    return {
+      props: data,
+    };
+  } catch (error) {
+    console.log(error);
+  }
 }
