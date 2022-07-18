65ea2333edbd4adce7755a503b7a6015e0fa9fe7
shaznan <shaznanfairozesally@gmail.com>
2022-07-18 18:45:47 +0530

    Make changes to next config to support additional ENV variables

--- next.config.js
+++ next.config.js
@@ -7,6 +7,10 @@ const nextConfig = {
   env: {
     REACT_APP_USER_POOL_ID: process.env.REACT_APP_USER_POOL_ID,
     REACT_APP_CLIENT_ID: process.env.REACT_APP_CLIENT_ID,
+    AWS_COGNITO_REGION: process.env.AWS_COGNITO_REGION,
+    REACT_APP_COGNITO_IDENTITY_POOL_ID:
+      process.env.REACT_APP_COGNITO_IDENTITY_POOL_ID,
+    REACT_APP_WEB_CLIENT_ID: process.env.REACT_APP_WEB_CLIENT_ID,
   },
 };
 
