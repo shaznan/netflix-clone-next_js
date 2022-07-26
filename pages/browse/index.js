5379e3fe6bd890991c9eb816abed4c75c36e8eca
shaznan <shaznanfairozesally@gmail.com>
2022-07-27 00:36:06 +0530

    AWS download: Functionality test

--- pages/browse/index.js
+++ pages/browse/index.js
@@ -4,27 +4,28 @@ import { MainWrapper } from "../../components/Browse/styles";
 import { Storage, Auth } from "aws-amplify";
 
 const Browse = () => {
+  const requestConfig = {
+    customPrefix: {
+      public: "",
+      protected: "",
+      private: "",
+    },
+    level: "public",
+    cacheControl: "no-cache",
+  };
+
   const getURL = async () => {
     const result = await Storage.get(`Brooklyn-Nine-Nine_trailer.mp4`, {
+      level: "public",
       download: false,
       contentType: "video/mp4",
     });
-
-    const requestConfig = {
-      customPrefix: {
-        public: "",
-        protected: "",
-        private: "",
-      },
-      level: "public",
-      cacheControl: "no-cache",
-    };
 
     await Storage.list("", requestConfig)
       .then((result) => console.log("Result:", result)) // {key: "test.txt"}
       .catch((err) => console.log(err));
     // console.log(await Storage.list(), "LIST");
-    // console.log(result, "RESULT");
+    console.log(result, "RESULT");
     // console.log(
     //   "https://netflixclone9a89a58844304fa6925e71ef49ab0718133042-dev.s3.amazonaws.com/Brooklyn-Nine-Nine_trailer.mp4",
     //   "TRY"
