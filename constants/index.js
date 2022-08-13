f9be6d71398072b29c12db52a9a87782b8e46dd5
shaznan <shaznanfairozesally@gmail.com>
2022-08-13 13:55:09 +0530

    Add constant variables for movieTitles

--- constants/index.js
+++ constants/index.js
@@ -20,3 +20,19 @@ export const headerTheme = {
   LIGHT: "light",
   DARK: "dark",
 };
+
+export const movieSectionTitles = {
+  TRENDING_NOW: "Trending_Now",
+  AWARD_WINNING_US_TV_COMEDIES: "Award_Winning_US_TV_Comedies",
+  ONLY_ON_NETFLIX: "Only_on_Netflix",
+  HOLLYWOOD_MOVIES: "Hollywood_Movies",
+  COMEDIES: "Comedies",
+  NEW_RELEASES: "New_Releases",
+  WATCH_TOGETHER_FOR_OLDER_KIDS: "Watch_Together_for_Older_Kids",
+  WATCH_IT_AGAIN: "Watch It Again",
+  EXCITING_MOVIES: "Exciting_movies",
+  MOVIES_FROM_1990: "Movies_from_the_1990's",
+  AWARD_WINNING_FILMS: "Award Winning Films",
+  CRIME_COMEDIES: "Crime Comedies",
+  HOLLYWOOD_DRAMA_MOVIES: "Hollywood Drama Movies",
+};
--- containers/MovieContainer.js
+++ containers/MovieContainer.js
@@ -1,7 +1,7 @@
 import React from "react";
 import MovieThumbnailRow from "../components/common/MovieThumbnailRow/index";
 import styled from "styled-components";
-import { breakPoints_px } from "../constants";
+import { breakPoints_px, movieSectionTitles } from "../constants";
 
 const Container = styled.div`
   margin-top: -12vw;
@@ -32,20 +32,64 @@ const MovieContainer = () => {
     "https://netflix-clone-project.s3.amazonaws.com/public-directory/test/Project+Pictures/thumbnail__boJack_horseman.jpg",
   ];
 
-  //TODO:
-  /**
-   * show navigation arrow only when row is hovered, make it small, and scale it on hover
-   * bring the thumbnails to the left on initial render feels a bit off aligned
-   * add a seperate container or component for the hero section and make that height 100%
-   * scroll issue
-   */
+  const formatTitle = (title) => {
+    return title.replace(/_/g, " ");
+  };
+
   return (
     <Container>
-      <MovieThumbnailRow movies={movies} title="Trending Now" />
-      <MovieThumbnailRow movies={movies} title="Award-Winning US TV Comedies" />
-      <MovieThumbnailRow movies={movies} title="Award-Winning US TV Comedies" />
-      <MovieThumbnailRow movies={movies} title="Award-Winning US TV Comedies" />
-      <MovieThumbnailRow movies={movies} title="Award-Winning US TV Comedies" />
+      <MovieThumbnailRow
+        movies={movies}
+        title={formatTitle(movieSectionTitles.TRENDING_NOW)}
+      />
+      <MovieThumbnailRow
+        movies={movies}
+        title={formatTitle(movieSectionTitles.AWARD_WINNING_US_TV_COMEDIES)}
+      />
+      <MovieThumbnailRow
+        movies={movies}
+        title={formatTitle(movieSectionTitles.ONLY_ON_NETFLIX)}
+      />
+      <MovieThumbnailRow
+        movies={movies}
+        title={formatTitle(movieSectionTitles.HOLLYWOOD_MOVIES)}
+      />
+      <MovieThumbnailRow
+        movies={movies}
+        title={formatTitle(movieSectionTitles.COMEDIES)}
+      />
+      <MovieThumbnailRow
+        movies={movies}
+        title={formatTitle(movieSectionTitles.NEW_RELEASES)}
+      />
+      <MovieThumbnailRow
+        movies={movies}
+        title={formatTitle(movieSectionTitles.WATCH_TOGETHER_FOR_OLDER_KIDS)}
+      />
+      <MovieThumbnailRow
+        movies={movies}
+        title={formatTitle(movieSectionTitles.WATCH_IT_AGAIN)}
+      />
+      <MovieThumbnailRow
+        movies={movies}
+        title={formatTitle(movieSectionTitles.EXCITING_MOVIES)}
+      />
+      <MovieThumbnailRow
+        movies={movies}
+        title={formatTitle(movieSectionTitles.MOVIES_FROM_1990)}
+      />
+      <MovieThumbnailRow
+        movies={movies}
+        title={formatTitle(movieSectionTitles.AWARD_WINNING_FILMS)}
+      />
+      <MovieThumbnailRow
+        movies={movies}
+        title={formatTitle(movieSectionTitles.CRIME_COMEDIES)}
+      />
+      <MovieThumbnailRow
+        movies={movies}
+        title={formatTitle(movieSectionTitles.HOLLYWOOD_DRAMA_MOVIES)}
+      />
     </Container>
   );
 };
