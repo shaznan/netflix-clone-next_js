0abd44431e7d293724a55b13f79d5629e22b7560
shaznan <shaznanfairozesally@gmail.com>
2022-08-20 18:33:40 +0530

    Add signout dropdown in header section

--- components/Browse/Header/Avatar.js
+++ components/Browse/Header/Avatar.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import Image from "next/image";
 import AvatarImage from "../../../public/images/Netflix-avatar.png";
 import styled from "styled-components";
@@ -8,6 +8,7 @@ const RenderIcon = styled(MdPlayArrow)`
   color: white;
   transform: rotate(90deg);
   margin-left: 0.3rem;
+  cursor: pointer;
 `;
 
 const RenderImage = ({ className }) => (
@@ -27,11 +28,43 @@ const StyledImage = styled(RenderImage)`
   margin-left: 1rem;
 `;
 
+const Dropdown = styled.div`
+  position: relative;
+  span {
+    position: absolute;
+    right: 0;
+    top: 20px;
+    font-size: 14px;
+    color: white;
+    background-color: rgb(20, 20, 20);
+    padding: 0.5rem 1rem;
+    white-space: nowrap;
+    overflow: hidden;
+    border-radius: 5px;
+    cursor: pointer;
+    transition: 0.1s linear;
+    color: rgba(255, 255, 255, 0.8);
+
+    &:hover {
+      color: rgba(255, 255, 255, 1);
+    }
+  }
+`;
+
 const Avatar = ({ size = "large" }) => {
+  const [showDropdown, setShowDropdown] = useState(false);
+
+  //TODO: Need to handle showDropdown
+
   return (
     <>
       <StyledImage size={size} />
-      <RenderIcon />
+      <RenderIcon onClick={() => setShowDropdown((prevState) => !prevState)} />
+      {showDropdown && (
+        <Dropdown onMouseLeave={() => setShowDropdown(false)}>
+          <span>Sign out</span>
+        </Dropdown>
+      )}
     </>
   );
 };
