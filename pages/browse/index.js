c08f256898c443792fcbcebc4035ab4d66b4186e
shaznan <shaznanfairozesally@gmail.com>
2022-08-20 17:12:17 +0530

    Protect Browse route by redirecting unAuth users to homepage

--- pages/browse/index.js
+++ pages/browse/index.js
@@ -1,10 +1,12 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useMemo } from "react";
 import Header from "../../components/Browse/Header";
 import { MainWrapper } from "../../components/Browse/styles";
 import { Storage, Auth } from "aws-amplify";
 import Hero from "../../components/Browse/Hero/index";
 import MovieThumbnailRow from "../../components/common/MovieThumbnailRow";
 import MovieContainer from "../../containers/MovieContainer";
+import { useSelector } from "react-redux";
+import { useRouter } from "next/router";
 
 Storage.configure({ level: "public" });
 
@@ -17,6 +19,18 @@ Storage.configure({ level: "public" });
  */
 
 const Browse = () => {
+  const router = useRouter();
+  const { userData } = useSelector((state) => state.Auth);
+  const isUserAuthenticated = useMemo(
+    () => userData?.signInUserSession?.idToken?.jwtToken?.length,
+    [JSON.stringify(userData)]
+  );
+
+  if (!isUserAuthenticated) {
+    router.push("/");
+    return;
+  }
+
   return (
     <MainWrapper>
       <Header />
